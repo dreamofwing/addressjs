498a7ae5f310b68102853d450681bb086ca66df7
uijun Lim <dreamofwing@gmail.com>
2015-08-08 14:23:17 +0900

    default set

--- jquery.address-0.1.js
+++ jquery.address-0.1.js
@@ -1,4 +1,36 @@
 ;(function ($, window, document, undefined){
-	
+
+	// 기본갑
+	var pluginName = 'address',
+		defaults = {
+			rows: "100"
+		};
+
+	//생성자
+	function Address (element, options){
+		this.element = element;
+
+		this.options = $.extend({}, defaults, options);
+		this._defaults = defaults;
+		this._name = plginName;
+
+		this.init();
+		
+	}
+
+
+	// 초기화 함수
+	Address.prototype.init = function(){
+		
+	}
+
+
+	$.fn[pluginName] = function(options) {
+		return this.each(function(){
+			if(!$.data(this, 'plugin_' + pluginName)){
+				$.data(this, 'plugin_' + pluginName, new Address(this, options));
+			}
+		})
+	}
 
 })(jQuery, window, document);
